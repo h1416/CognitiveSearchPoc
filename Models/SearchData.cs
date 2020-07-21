3961ec6ec6545672530f275a00eb9fda6308f074
HV <hieuvo1416@gmail.com>
2020-07-21 16:59:55 -0500

    Add project files.

---
+++ Models/SearchData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Search.Models;
+
+namespace AzureSearchPoc.Models
+{
+    public class SearchData
+    {
+        // The text to search for.
+        public string searchText { get; set; }
+
+        // The list of results.
+        public DocumentSearchResult<Hotel> resultList;
+    }
+
+}
