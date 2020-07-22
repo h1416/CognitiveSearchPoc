f9bb50758ff4e5daef63506d03fd5e56cc63cc04
HV <hieuvo1416@gmail.com>
2020-07-22 08:43:16 -0500

    Added UI for basic search bar. It's functional by this stage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AzureSearchPoc.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
 
 namespace AzureSearchPoc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static SearchServiceClient _serviceClient;
+        private static ISearchIndexClient _indexClient;
+        private static IConfigurationBuilder _builder;
+        private static IConfigurationRoot _configuration;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,6 +27,29 @@ namespace AzureSearchPoc.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the Azure Cognitive Search call.
+                await RunQueryAsync(model);
+            }
+
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+            return View(model);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
@@ -33,5 +60,39 @@ namespace AzureSearchPoc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        private void InitSearch()
+        {
+            // Create a configuration using the appsettings file.
+            _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _configuration = _builder.Build();
+
+            // Pull the values from the appsettings.json file.
+            string searchServiceName = _configuration["SearchServiceName"];
+            string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+
+            // Create a service and index client.
+            _serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(queryApiKey));
+            _indexClient = _serviceClient.Indexes.GetClient("hotels");
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model)
+        {
+            InitSearch();
+
+            var parameters = new SearchParameters
+            {
+                // Enter Hotel property names into this list so only these values will be returned.
+                // If Select is empty, all values will be returned, which can be inefficient.
+                Select = new[] { "HotelName", "Description" }
+            };
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _indexClient.Documents.SearchAsync<Hotel>(model.searchText, parameters);
+
+            // Display the results.
+            return View("Index", model);
+        }
+
     }
 }
