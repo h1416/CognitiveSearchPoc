1b7583ae78a13cb0f7dc4b7695b9dc539ee74f7b
HV <hieuvo1416@gmail.com>
2020-07-22 11:26:34 -0500

    Revert "Add infinite scrolling"
    This reverts commit 13b6fee9625429e8778969c765855d6357bed8f8.

--- Models/SearchData.cs
+++ Models/SearchData.cs
@@ -19,14 +19,7 @@ namespace AzureSearchPoc.Models
         // The number of page numbers to display - which can be less than MaxPageRange towards the end of the results.
         public int pageRange { get; set; }
 
-        /// <summary>
-        /// This is for paging option
-        /// </summary>
-        //// Used when page numbers, or next or prev buttons, have been selected.
-        //public string paging { get; set; }
-
-        //This is for infinite scrolling option
-        // Record if the next page is requested.
+        // Used when page numbers, or next or prev buttons, have been selected.
         public string paging { get; set; }
 
         // The list of results.
